9e38cf4453e674d9e3195cd76a303a532dbb7e3b
HansolEom <45479160+HansolEom@users.noreply.github.com>
2021-07-09 14:40:00 +0900

    Update detection_datasets.py

---
+++ imre/dataset/detection_datasets.py
@@ -0,0 +1,54 @@
+import torch
+import torchvision
+from module.utils import BoxList
+
+class COCODataset(torchvision.datasets.coco.CocoDetection):
+    def __init__(
+        self, ann_file, root, transforms=None
+    ):
+        super(COCODataset, self).__init__(root, ann_file)
+        # sort indices for reproducible results
+        self.ids = sorted(self.ids)
+
+        self.json_category_id_to_contiguous_id = {
+            v: i + 1 for i, v in enumerate(self.coco.getCatIds())
+        }
+        self.contiguous_category_id_to_json_id = {
+            v: k for k, v in self.json_category_id_to_contiguous_id.items()
+        }
+        self.id_to_img_map = {k: v for k, v in enumerate(self.ids)}
+        self._transforms = transforms
+
+    def __getitem__(self, idx):
+        img, anno = super(COCODataset, self).__getitem__(idx)
+
+        anno = [obj for obj in anno if obj["iscrowd"] == 0]
+
+        boxes = [obj["bbox"] for obj in anno]
+        boxes = torch.as_tensor(boxes).reshape(-1, 4)  # guard against no boxes
+        target = BoxList(boxes, img.size, mode="xywh").convert("xyxy")
+
+        classes = [obj["category_id"] for obj in anno]
+        classes = [self.json_category_id_to_contiguous_id[c] for c in classes]
+        classes = torch.tensor(classes)
+        target.add_field("labels", classes)
+
+        if self._transforms is not None:
+            img, target = self._transforms(img, target)
+
+        return img, target, idx
+
+    def get_img_info(self, index):
+        img_id = self.id_to_img_map[index]
+        img_data = self.coco.imgs[img_id]
+        return img_data
+
+
+if __name__ == "__main__":
+
+    valid_dataset = COCODataset("/Users/hansoleom/Desktop/deepfashion2/deepfashion2/valid.json", "/Users/hansoleom/Desktop/deepfashion2/deepfashion2/valid")
+    img, target, idx = next(iter(valid_dataset))
+    print(img)
+    print(target)
+    print(idx)
+    print(len(valid_dataset))
